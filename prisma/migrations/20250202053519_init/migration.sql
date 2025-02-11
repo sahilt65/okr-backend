8bbfb605adbcdd3b9e564ab6f8ee822d2d895eb9
sahilt65 <sahiltpict@gmail.com>
2025-02-11 17:20:46 +0530

    add: changes in package.json

--- prisma/migrations/20250202053519_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "KeyResult" DROP CONSTRAINT "KeyResult_objectiveID_fkey";
-
--- AddForeignKey
-ALTER TABLE "KeyResult" ADD CONSTRAINT "KeyResult_objectiveID_fkey" FOREIGN KEY ("objectiveID") REFERENCES "Objective"("id") ON DELETE CASCADE ON UPDATE CASCADE;
